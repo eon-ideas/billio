1279063cde61257c71165e645de6d378179869a9
Gordan Jugo <gordan.jugo@eonideas.hr>
2024-12-08 16:41:34 +0100

    fix: build production build err

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useAuthStore } from '@/stores/auth'
-import { useRouter } from 'vue-router'
 
 const auth = useAuthStore()
-const router = useRouter()
 
 const email = ref('')
 const password = ref('')
