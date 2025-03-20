2ff56f7a095836fccb4f86fb2fa5657d95a91968
Gordan Jugo <gordan.jugo@eonideas.hr>
2025-03-20 23:32:10 +0100

    Login background image moved to public folder

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -202,7 +202,7 @@ onMounted(() => {
 })
 
 const userPhotoUrl = computed(() => {
-  return 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  return '/img/user-avatar.jpg'
 })
 
 const topCustomers = computed(() => {
